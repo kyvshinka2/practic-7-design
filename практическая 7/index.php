030cadf3e4b87582c2e61f965111534f78cdab8e
kyvshinka2 <115142098+kyvshinka2@users.noreply.github.com>
2023-03-28 20:15:52 +0500

    Add files via upload

---
+++ практическая 7/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/header-footer.css">
+</head>
+<body>
+    <?php
+    include 'php-header-footer/header.php';
+    ?>
+
+    <!-- 1 блок -->
+    <div class="hero">
+        <div class="hashtag">
+            <p class="hash">#Gericht</p>
+            <p class="hash">#Bar</p>
+        </div>
+        <div class="content">
+            <div class="content-text">
+                <p class="chase">Chase the new Flavour</p>
+                <p>The key to Fine dining</p>
+                <p>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+                <input type="submit" value="Explore Menu">
+            </div>
+            <div class="content-img">
+                <img src="./img/Hero img.png">
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- шрифты -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright:wght@700&family=Open+Sans:wght@500&display=swap" rel="stylesheet">
+</body>
+</html>
---
+++ практическая 7/php-header-footer/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/main.css">
+    <link rel="stylesheet" href="./style/header-footer.css">
+</head>
+<body>
+    <header class="header">
+        <p class="logo-text">Gerícht</p>
+        <ul class="ul-with-header">
+            <li><a class="a-text-header" href="#">Home</a></li>
+            <li><a class="a-text-header" href="#">Pages</a></li>
+            <li><a class="a-text-header" href="#">Contact Us</a></li>
+            <li><a class="a-text-header" href="#">Blog</a></li>
+            <li><a class="a-text-header" href="#">Landing</a></li>
+        </ul>
+        <ul class="ul-with-header-2">
+            <li><a class="a-text-header" href="#">Log in / registration</a></li>
+            <li><a class="a-text-header" href="#">Book table</a></li>
+        </ul>
+    </header>
+
+    <!-- шрифты -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright:wght@700&family=Open+Sans:wght@500&display=swap" rel="stylesheet">
+</body>
+</html>
